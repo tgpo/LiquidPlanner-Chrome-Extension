5a0ba27bffb3d84037defee5cabff24d3fc6ecdd
tgpo <tgpotgpo@gmail.com>
2013-01-22 21:48:50 -0500

    Updated notifications, optimized code, fixed 1st run bug

--- js/background.js
+++ js/background.js
@@ -1,39 +1,44 @@
 function init() {
-  if (!localStorage.comments) {
+	if (!localStorage.comments) {
 		localStorage.comments = "";
 	}
 	
 	var ready = false;
 	
-	var intervalReady = window.setInterval(checkReady, 5000); 
-	
-	if(ready){
+	intervalReady = setInterval(function(){checkReady()}, 500); 
+}
+
+function beginCheckingComments(){
+		clearInterval(intervalReady);
 		getComments();
 		var intervalID = window.setInterval(getComments, 5000); 
-	  }
-	
 }
 
 function checkReady() {
 	if(LiquidPlanner.isConfigured()) {
 		ready = true;
+		beginCheckingComments();
 	} else {
 		ready = false;
 	}
 }
-
+var intervalReady,memberList;
+memberList = {};
 init();
 
 
 function commentChecker(comments) {
-	var commentID, allCommentIDs,text;
+	var commentID, allCommentIDs,text,username;
 	allCommentIDs = "0";
     
 	for (var i = 0, comment; comment = comments[i]; i++) {
 		commentID   = comment.id + ":";
 		allCommentIDs = localStorage.comments;
-		author = LiquidPlanner.members[comment.member_id] || LiquidPlanner.members[0];
+		author = memberList[comment.member_id] || memberList[0];
+		username = author.user_name;
+		avatar = 'https://app.liquidplanner.com' + author.avatar_url;
 		task   = comment.treeitem;
+		
 		
 		
 		if(allCommentIDs.indexOf(commentID) != -1) {
@@ -42,29 +47,63 @@ function commentChecker(comments) {
 		
 			// Create a simple text notification:
 			var notification = webkitNotifications.createNotification(
-				'images/icon-48.png',  // icon url - can be relative
-			  'Liquid Planner Comment Posted',  // notification title
-			  text = comment.plain_text.slice(0,72) + author + task.name
+				avatar,  // icon url - can be relative
+			  username + ' posted: ',  // notification title
+			  text = comment.plain_text.slice(0,100)
 
 			  // notification body text
 			);
+			
+			var myURL = route(LiquidPlanner.showTaskUrl, {task_id: comment.item_id})
+			
+			
+			notification.onclick = function(event) {
+				var args = { 
+					url: myURL, 
+					active : true 
+				}; 
+				event.currentTarget.cancel()
+				
+				chrome.tabs.create(args, null);
+			}
+			
+			//notification.ondisplay = function(event) {
+            //            setTimeout(function() {
+            //                event.currentTarget.cancel();
+            //            }, 10 * 1000);
+            //        }
 			
 			notification.show();
 			localStorage.comments = allCommentIDs + commentID;
 		}
   }
   
-  //alert(allCommentIDs);
+}
+
+// Stores the workspace members as a hash.  
+// These are used to display the member's name in comments the comments section.
+function storeMembers(members){
+  for (var i = 0, member; member = members[i]; i++) {
+    memberList[member.id] = member;
+  }
 }
 
 function getComments(){
   if(LiquidPlanner.defaults.commentCount > 0){
-		LiquidPlanner.chatter({
-		  success:  commentChecker,
-		  data:{
-			'ignore_mine': LiquidPlanner.defaults.ignoreMyComments,
-			'for_me':      !LiquidPlanner.defaults.showAllComments,
-			'limit':       LiquidPlanner.defaults.commentCount
-		}});
+      LiquidPlanner.members({
+      timeout:  3500,
+      success: function(members){
+		storeMembers(members);
+        LiquidPlanner.chatter({
+          success:  commentChecker,
+          data:{
+            'ignore_mine': LiquidPlanner.defaults.ignoreMyComments,
+            'for_me':      !LiquidPlanner.defaults.showAllComments,
+            'limit':       LiquidPlanner.defaults.commentCount
+        }});
+      }
+    });
+	
+
 	}
 }
--- js/popup.js
+++ js/popup.js
@@ -68,7 +68,7 @@ function showComments(comments) {
     }
     
     chatter.append(html);
-  
+	
 
   }
 }
